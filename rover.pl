c33d7e7cb919b6d17dcb61293eb847d547c77a7f
Michiel Vandendriessche <michielvdd@me.com>
2016-01-03 18:44:10 +0100

    er werkt al iets meer

--- rover.pl
+++ rover.pl
@@ -3,8 +3,10 @@
 board_x_max(4).
 board_y_max(4).
 
-currentPosition(position(2, 2), s0).
+currentPosition(position(0, 0), s0).
 destination(position(4, 4), s0).
+
+pit(position(2,2)).
 
 nextDestination(position(X, Y), S) :- destination(position(X, Y), S).
 
@@ -37,10 +39,10 @@ poss(turnoff(position(X, Y)), S) :-
 % Successor state axioms for primitive fluents
 
 currentPosition(position(K, Y), do(A, S)) :-
-  A = hmove(Z), currentPosition(position(X, Y), S), K is X + Z.
+  A = hmove(Z), currentPosition(position(X, Y), S), K is X + Z, \+ pit(position(K, Y)).
 
 currentPosition(position(X, K), do(A, S)) :-
-  A = vmove(Z), currentPosition(position(X, Y), S), K is Y + Z.
+  A = vmove(Z), currentPosition(position(X, Y), S), K is Y + Z, \+ pit(position(X, K)).
 
 currentPosition(position(X, Y), do(A, S)) :-
   A \= vmove(_), A \= hmove(_), currentPosition(position(X, Y), S).
@@ -57,7 +59,7 @@ horizontalPositionIsEqual(position(X,_), position(XX,_)) :- X = XX.
 
 %proc(move(N), (if(verticalPositionIsHigherThan(N, currentPosition(M)), vmove(-1), vmove(1)) # if(horizontalPositionIsHigherThan(N, currentPosition(M)), hmove(-1), hmove(1)))).
 %proc(move(N), (hmove(1) # vmove(1) # hmove(-1) # vmove(-1))).
-proc(move(N), pi(n, ?(currentPosition(n)) : (if(verticalPositionIsHigherThan(N, n), vmove(-1), if(verticalPositionIsEqual(N,n), if(horizontalPositionIsHigherThan(N, n), hmove(-1), hmove(1)), vmove(1)))))).
+proc(move(N), pi(n, ?(currentPosition(n)) : (if(verticalPositionIsHigherThan(N, n), (vmove(-1) # vmove(1) # hmove(1) # hmove(-1)), if(verticalPositionIsEqual(N,n), if(horizontalPositionIsHigherThan(N, n), (hmove(-1) # hmove(1) # vmove(-1) # vmove(1)), (hmove(1) # hmove(-1) # vmove(1) # vmove(-1))), (vmove(1) # vmove(-1) # hmove(1) # hmove(-1))))))).
 
 proc(goTo(N), while(-currentPosition(N), move(N))).
 proc(serve(N), goTo(N)).
