e05d4211d115c3888f0631812a8e403d6ef1f2cd
BavoGoosens <Bavo@TheBatMac.local>
2016-01-06 13:24:04 +0100

    stuff

--- Part 2/moetjeblok.pl
+++ Part 2/moetjeblok.pl
@@ -3,5 +3,9 @@
 % that. But it can sense things like whether a block is clear, whether a block
 % is on the floor, and whether a block is on the table. The end goal is to get
 % all the blocks on the table.
+%
+% for the full kb prolog program see slides and/or 
+% http://citeseerx.ist.psu.edu/viewdoc/download;jsessionid=350CC0AE649D0A16A3902B6BA73ADCF9?doi=10.1.1.16.8014&rep=rep1&type=pdf
+
 
 :- include(kplanner).
