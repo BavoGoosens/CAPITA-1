7e94d41b99060a96a643f96dcab3a81aefeccbc4
BavoGoosens <Bavo@TheBatMac.local>
2016-01-06 20:26:37 +0100

    jhsgdjgsj

--- Part 2/moetjeblok.pl
+++ Part 2/moetjeblok.pl
@@ -1,25 +1,25 @@
 
 :-include(kplanner).
 
-prim_action(putOnTable,[ok]).		% put the block on the table
+prim_action(putOnTable(_),[ok]).		% put the block on the table
 prim_action(look,[empty,nonEmpty]).	% look if the table is empty or non-empty
-prim_action(checkBlock,[clear, notClear, onTable]).	% check if the block is clear
+prim_action(checkBlock(_),[clear, notClear, onTable]).	% check if the block is clear
 
-prim_fluent(block).         % clear of not clear
+prim_fluent(block(_)).       % clear of not clear
 prim_fluent(floor).	        % empty or non-empty
 prim_fluent(floorMax).	    % unknown bound on the number of blocks on the floor
 
-poss(putOnTable,block=clear).
+poss(putOnTable(B),block(B)=clear).
 poss(look,true).
-poss(checkBlock,true).
+poss(checkBlock(_),true).
 
 % causes(A,R,F,V,W), is used to state that action A changes the value of F.
 %    Specifically, if A returns result R, then the possible values for F are
 %    any value V for which W is true.
 %          e.g. causes(walk_to(X),_,mylocation,X,true).
 %               causes(apply_heat,_,temperature,X,X is temperature+1).
-causes(putOnTable, floorMax,X,X is floorMax-1).
-causes(putOnTable, block, onTable, true).
+causes(putOnTable(_), floorMax,X,X is floorMax-1).
+causes(putOnTable(B), block(B), onTable, true).
 % causes(putOnTable,tree,down,true).
 % causes(putOnTable,tree,up,true).
 
@@ -30,9 +30,9 @@ settles(look,empty,floorMax,0,true).
 rejects(look,nonEmpty,floorMax,0,true).
 
 % checkBlock determines whether a block is clear, notClear or onTable.
-settles(checkBlock,X, block, X, true).
+settles(checkBlock(B),X, block(B), X, true).
 
-init(block,clear).      % the axe is out and available
+init(block(_),clear).      % the axe is out and available
 init(floor,nonEmpty).      % the tree may be up initially
 init(floor,empty).    % the tree may be down  initially
 
