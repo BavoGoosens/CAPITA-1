ea0f3bfdde9290fa755e06f720053d158669b5a6
BavoGoosens <Bavo@TheBatMac.local>
2016-01-06 13:19:08 +0100

    stuff

--- Part 2/moetjeboom.pl
+++ Part 2/moetjeboom.pl
@@ -1,2 +1,7 @@
+%%  Puts some blocks on the table
+% The robot has some basic knowledge (e.g. how many blocks) but not much beyond
+% that. But it can sense things like whether a block is clear, whether a block
+% is on the floor, and whether a block is on the table. The end goal is to get
+% all the blocks on the table.
 
 :- include(kplanner).
