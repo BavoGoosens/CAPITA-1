15d7cbc554d282b404624f52a94e00e3e1dc0500
BavoGoosens <Bavo@TheBatMac.local>
2016-01-06 13:34:41 +0100

    stuff

--- Part 2/moetjeblok.pl
+++ Part 2/moetjeblok.pl
@@ -4,8 +4,13 @@
 % is on the floor, and whether a block is on the table. The end goal is to get
 % all the blocks on the table.
 %
-% for the full kb prolog program see slides and/or 
+% for the full kb prolog program see slides and/or
 % http://citeseerx.ist.psu.edu/viewdoc/download;jsessionid=350CC0AE649D0A16A3902B6BA73ADCF9?doi=10.1.1.16.8014&rep=rep1&type=pdf
 
 
 :- include(kplanner).
+
+% sensing actions
+prim_action(clear, [clear, notclear]).
+prim_action(onfloor, [onthefloor, notonthefloor]).
+prim_action(ontable, [onthetable, notonthetable]).
