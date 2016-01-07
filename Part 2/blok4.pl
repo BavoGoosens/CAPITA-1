31105413aa9187395ce7ddd8e99c52fd060dbb5c
Michiel Vandendriessche <michielvdd@me.com>
2016-01-07 11:15:04 +0100

    stuff

--- Part 2/blok4.pl
+++ Part 2/blok4.pl
@@ -5,35 +5,36 @@
 
 prim_action(pickupBlock,[ok]).		% hit the floor with the axe
 prim_action(look,[empty,notEmpty]).	% look if the floor is notEmpty or empty
-%prim_action(store,[ok]).	% put away the axe
+prim_action(jezza,[jos,jef]).
+prim_action(disregard, [ok]).
 
-%prim_fluent(axe).               % stored or out
 prim_fluent(floor).	        % notEmpty or empty
 prim_fluent(blocksOnFloor).	        % unknown bound on the number of chops
+prim_fluent(block).              % jos or jef
 
-poss(pickupBlock,floor=notEmpty).
+poss(pickupBlock,and(floor=notEmpty, block=jos)).
+poss(disregard, block=jef).
 poss(look,true).
-%poss(store,axe=out).
+poss(jezza,true).
 
-% causes(A,R,F,V,W), is used to state that action A changes the value of F.
-%    Specifically, if A returns result R, then the possible values for F are
-%    any value V for which W is true.
-%          e.g. causes(walk_to(X),_,mylocation,X,true).
-%               causes(apply_heat,_,temperature,X,X is temperature+1).
-%causes(store,axe,stored,true).
 causes(pickupBlock,blocksOnFloor,X,X is blocksOnFloor-1).
 causes(pickupBlock,floor,empty,true).
 causes(pickupBlock,floor,notEmpty,true).
 
+causes(disregard, block, jos, true).
+causes(disregard, block, jef, true).
+
+settles(jezza,X,block,X,true).
 % looking determines whether the floor is notEmpty or empty
 settles(look,X,floor,X,true).
 % if the floor is seen to be notEmpty, blocksOnFloor cannot be 0
 settles(look,empty,blocksOnFloor,0,true).
 rejects(look,notEmpty,blocksOnFloor,0,true).
 
-%init(axe,out).      % the axe is out and available
 init(floor,notEmpty).      % the floor may be notEmpty initially
 init(floor,empty).    % the floor may be empty  initially
+init(block,jos).
+init(block,jef).
 
 parm_fluent(blocksOnFloor).           % blocksOnFloor is the unique parameter
 init_parm(generate,blocksOnFloor,1).  % small bound for generating is 1
